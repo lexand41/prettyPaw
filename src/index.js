87d6ea319719a61cbecb4aea3fcec62c8e0b0d3d
lexand41 <lexand41@yandex.ru>
2023-05-10 14:13:59 +0700

    add controlMenu + hover, focus effects

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import 'swiper/scss/pagination'
 import './index.scss';
 import { slidersInit } from './modules/sliders';
 import { videoBackgroundInit } from './modules/videoBackground';
+import { menuControl } from './modules/menuControl';
 
 
 // use modules
 videoBackgroundInit();
+menuControl();
 
 slidersInit('.about__slider', {
   pagination: {
